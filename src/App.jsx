ce3044be772347a1c75a0a227ccefe586ef9d27e
Aayush Bhusari <bhusariaayush29@gmail.com>
2025-02-24 00:30:02 +0530

    Added Particle BG Effect

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,24 @@ import { Route, Routes } from "react-router-dom";
 import Layout from "./Layout";
 import ErrorPage from "./pages/ErrorPage";
 import NewPage from "./pages/NewPage";
+import ParticlesComponent from "./components/particles/particles";
 import AttendeeCertificate from "./pages/AttendeeCertificate";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/">
-        <Route index element={<NewPage />} />
-        <Route path="*" element={<ErrorPage />} />
+    <>
+      <ParticlesComponent id="particles" />
+      <Routes>
+        <Route path="/">
+          <Route index element={<NewPage />} />
+          <Route path="*" element={<ErrorPage />} />
 
-        <Route path="certificate" element={<Layout />}>
-          <Route path="attendee/:refID" element={<AttendeeCertificate />} />
+          <Route path="certificate" element={<Layout />}>
+            <Route path="attendee/:refID" element={<AttendeeCertificate />} />
+          </Route>
         </Route>
-      </Route>
-    </Routes>
+      </Routes>
+    </>
   );
 }
 
---
+++ src/components/particles/particles.jsx
@@ -0,0 +1,102 @@
+import Particles, { initParticlesEngine } from "@tsparticles/react";
+import { useEffect, useMemo, useState } from "react";
+import PropTypes from "prop-types"; // Added missing import
+import { loadSlim } from "@tsparticles/slim";
+
+const ParticlesComponent = ({ id }) => {
+  // eslint-disable-next-line no-unused-vars
+  const [init, setInit] = useState(false);
+
+  useEffect(() => {
+    initParticlesEngine(async (engine) => {
+      await loadSlim(engine);
+    }).then(() => {
+      setInit(true);
+    });
+  }, []);
+
+  const particlesLoaded = (container) => {
+    console.log("Particles loaded:", container);
+  };
+
+  const options = useMemo(
+    () => ({
+      background: {
+        color: {
+          value: "#000",
+        },
+      },
+      fpsLimit: 120,
+      interactivity: {
+        events: {
+          onClick: {
+            enable: true,
+            mode: "repulse",
+          },
+          onHover: {
+            enable: true,
+            mode: "grab",
+          },
+        },
+        modes: {
+          push: {
+            distance: 200,
+            duration: 15,
+          },
+          grab: {
+            distance: 150,
+          },
+        },
+      },
+      particles: {
+        color: {
+          value: "#FFFFFF",
+        },
+        links: {
+          color: "#FFFFFF",
+          distance: 150,
+          enable: true,
+          opacity: 0.3,
+          width: 1,
+        },
+        move: {
+          direction: "none",
+          enable: true,
+          outModes: {
+            default: "bounce",
+          },
+          random: true,
+          speed: 1,
+          straight: false,
+        },
+        number: {
+          density: {
+            enable: true,
+          },
+          value: 150,
+        },
+        opacity: {
+          value: 1.0,
+        },
+        shape: {
+          type: "circle",
+        },
+        size: {
+          value: { min: 1, max: 3 },
+        },
+      },
+      detectRetina: true,
+    }),
+    []
+  );
+
+  return (
+    <Particles id={id} particlesLoaded={particlesLoaded} options={options} />
+  );
+};
+
+ParticlesComponent.propTypes = {
+  id: PropTypes.string.isRequired,
+};
+
+export default ParticlesComponent;
